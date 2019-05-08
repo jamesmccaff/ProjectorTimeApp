b55cf7731e4a14425bb0cf6d988e699cff6fe07b
James-McDermott <james.mcdermott1101@gmail.com>
2019-05-08 15:24:12 +0100

    Removed unneeded methods from APIHelper leftoever from initial copy over

--- TimeImportApp/APIHelper.cs
+++ TimeImportApp/APIHelper.cs
@@ -78,100 +78,6 @@ namespace TimeImportApp
             return rs.SessionTicket;
         }
 
-        public PwsSaveTimeCardsRs SaveTimeCard(string sessionKey, PwsTimecardDetail timeCard, PwsUserElement userDetails)
-        {
-            PwsTimecardDetail[] timeCards = { timeCard };
-            PwsSaveTimeCardsRq timeCardsRq = new PwsSaveTimeCardsRq();
-            timeCardsRq.SessionTicket = sessionKey;
-            timeCardsRq.SaveTimeCards = timeCards;
-            timeCardsRq.ResourceIdentity = userDetails.ResourceIdentity;
-            timeCardsRq.StartDate = DateTime.Parse("2019-04-24T00:00:00Z").ToUniversalTime();
-            timeCardsRq.EndDate = DateTime.Parse("2019-04-25T00:00:00Z").ToUniversalTime();
-            return pwsProjectorServices.PwsSaveTimeCards(timeCardsRq);
-        }
-
-        public PwsTimecardDetail GenerateNewTimeCard()
-        {
-            //Create a new time card - WIP
-            PwsTimecardDetail pwsTimecardDetail = new PwsTimecardDetail();
-            //For a new timecard to be valid the below must be set as a minimum
-            pwsTimecardDetail.WorkMinutes = 450;
-            pwsTimecardDetail.WorkDate = DateTime.Parse("2019-04-24T00:00:00Z").ToUniversalTime();
-            pwsTimecardDetail.CardStatus = "D";
-
-            //Also needs the below, each needing at least one of the ID types
-            PwsProjectRef pwsProjectRef = new PwsProjectRef();
-            pwsProjectRef.ProjectCode = "";
-            PwsProjectRateTypeRef pwsProjectRateTypeRef = new PwsProjectRateTypeRef();
-            pwsProjectRateTypeRef.ExternalSystemIdentifier = "";
-            PwsProjectTaskRef pwsProjectTaskRef = new PwsProjectTaskRef();
-            pwsProjectTaskRef.ExternalSystemIdentifier = "";
-            PwsProjectRoleRef pwsProjectRoleRef = new PwsProjectRoleRef();
-            pwsProjectRoleRef.ExternalSystemIdentifier = "";
-
-            pwsTimecardDetail.ProjectIdentity = pwsProjectRef;
-            pwsTimecardDetail.ProjectRateTypeIdentity = pwsProjectRateTypeRef;
-            pwsTimecardDetail.ProjectTaskIdentity = pwsProjectTaskRef;
-            pwsTimecardDetail.RoleIdentity = pwsProjectRoleRef;
-            return pwsTimecardDetail;
-        }
-
-        public int CalculateTimeOffInYearToDate(PwsGetTimeCardsRs timeCard)
-        {
-            //Iterate through time off projects and total YTD timeoff
-            PwsTimeEntryTimeOff[] pwsTimeEntryProject = timeCard.TimeEntryTimeOff;
-            int minYtd = 0;
-
-            foreach (PwsTimeEntryTimeOff timeOff in pwsTimeEntryProject)
-            {
-                minYtd += timeOff.MinutesYearToDate;
-            }
-
-            return minYtd;
-        }
-
-        public PwsGetTimeCardsRs GetExistingTimeCard(string sessionKey, PwsUserElement userDetails)
-        {
-            //Create timecard request
-            PwsGetTimeCardsRq timeCardsRq = new PwsGetTimeCardsRq();
-            timeCardsRq.SessionTicket = sessionKey;
-            timeCardsRq.StartDate = DateTime.Parse("2019-04-24T00:00:00Z").ToUniversalTime();
-            timeCardsRq.EndDate = DateTime.Parse("2019-04-25T00:00:00Z").ToUniversalTime();
-            timeCardsRq.ResourceIdentity = userDetails.ResourceIdentity;
-
-            //Send request and check response status
-            return pwsProjectorServices.PwsGetTimeCards(timeCardsRq);
-        }
-
-        public string GenerateSessionKey(IPwsProjectorServices pwsProjectorServices, string username, string password, string accountCode)
-        {
-            //Authenticate user and print session key
-            Session session = new Session(ref pwsProjectorServices, accountCode, username, password);
-            string sessionKey = session.GetSessionTicket();
-            return sessionKey;
-            //Using session key create and expense report request, and print out request status
-        }
-
-        public PwsGetExpenseReportsRs GetExpenseReport(string sessionKey)
-        {
-            //Using session key create and expense report request, and print out request status
-            PwsGetExpenseReportsRq pwsGetExpenseReportsRq = new PwsGetExpenseReportsRq();
-            pwsGetExpenseReportsRq.SessionTicket = sessionKey;
-            return pwsProjectorServices.PwsGetExpenseReports(pwsGetExpenseReportsRq);
-        }
-
-        public PwsUserElement GetUserDetails(string userName, string sessionKey)
-        {
-            PwsGetUserRq getUserRq = new PwsGetUserRq();
-            getUserRq.SessionTicket = sessionKey;
-            PwsUserRef userRef = new PwsUserRef();
-            userRef.UserDisplayName = userName;
-            PwsUserRef[] userArray = new PwsUserRef[] { userRef };
-            getUserRq.UserIdentities = userArray;
-            PwsGetUserRs getUserRs = pwsProjectorServices.PwsGetUser(getUserRq);
-            return getUserRs.Users[0];
-        }
-
         private PwsResourceElement[] GetProjectorResource(List<Person> person, string sessionTicket)
         {
             PwsResourceRef[] userList = new PwsResourceRef[person.Count];
@@ -204,8 +110,7 @@ namespace TimeImportApp
 
         private PwsTimecardDetail[] CreateTimeCards(Person person, string sessionTicket)
         {
-            PwsTimecardDetail[] timecardDetails = new PwsTimecardDetail[person.Jobs.Count( j => j.SoldHours > 0)];
-            int addedCards = 0;
+            List<PwsTimecardDetail> timecardDetails = new List<PwsTimecardDetail>();
             foreach (Job job in person.Jobs)
             {
                 if (job.SoldHours > 0)
@@ -220,8 +125,7 @@ namespace TimeImportApp
                         timeCard.ProjectTaskIdentity = new PwsProjectTaskRef() { ProjectTaskUid = pwsProject.Tasks[0].ProjectTaskDetail.ProjectTaskUid };
                         timeCard.WorkMinutes = (int)(job.SoldHours * 60);
                         //Date details need figured out and added here
-                        timecardDetails[addedCards] = timeCard;
-                        addedCards++;
+                        timecardDetails.Add(timeCard);
                     }
                     else
                     {
@@ -229,41 +133,40 @@ namespace TimeImportApp
                     }
                 }
             }
-            return timecardDetails;
+            return timecardDetails.ToArray();
         }
 
         private PwsTimeOffCardDetail[] CreateTimeOffCards(Person person, string sessionTicket)
         {
-            PwsTimeOffCardDetail[] pwsTimeOffCards = new PwsTimeOffCardDetail[person.Jobs.Count( j => (j.AnnualHolidayHours > 0) || (j.IllnessHours > 0))];
-            int addedCards = 0;
+            List<PwsTimeOffCardDetail> pwsTimeOffCards = new List<PwsTimeOffCardDetail>();
             foreach (Job job in person.Jobs)
             {
                 if (job.AnnualHolidayHours > 0)
                 {
                     PwsTimeOffCardDetail pwsTimeOffCardDetail = new PwsTimeOffCardDetail();
                     pwsTimeOffCardDetail.CardStatus = "D";
-                    pwsTimeOffCardDetail.WorkDate = DateTime.Now;
+                    pwsTimeOffCardDetail.WorkDate = DateTime.Now; //TODO: Replace with actual date data
                     pwsTimeOffCardDetail.WorkMinutes = (int)(job.AnnualHolidayHours * 60);
                     pwsTimeOffCardDetail.TimeOffReasonIdentity = new PwsTimeOffReasonRef()
                     {
                         TimeOffReasonName = "Vacation"
                     };
-                    pwsTimeOffCards[addedCards] = pwsTimeOffCardDetail;
+                    pwsTimeOffCards.Add(pwsTimeOffCardDetail);
                 }
                 else if (job.IllnessHours > 0)
                 {
                     PwsTimeOffCardDetail pwsTimeOffCardDetail = new PwsTimeOffCardDetail();
                     pwsTimeOffCardDetail.CardStatus = "D";
-                    pwsTimeOffCardDetail.WorkDate = DateTime.Now;
+                    pwsTimeOffCardDetail.WorkDate = DateTime.Now; //TODO: Replace with actual date data
                     pwsTimeOffCardDetail.WorkMinutes = (int)(job.IllnessHours * 60);
                     pwsTimeOffCardDetail.TimeOffReasonIdentity = new PwsTimeOffReasonRef()
                     {
                         TimeOffReasonName = "Sick"
                     };
-                    pwsTimeOffCards[addedCards] = pwsTimeOffCardDetail;
+                    pwsTimeOffCards.Add(pwsTimeOffCardDetail);
                 }
             }
-            return pwsTimeOffCards;
+            return pwsTimeOffCards.ToArray();
         }
 
         private PwsSaveTimeCardsRs SaveTimeCards(PwsTimecardDetail[] timeCards, PwsTimeOffCardDetail[] timeOffCards, PwsResourceRef user, string sessionTicket)
@@ -288,7 +191,7 @@ namespace TimeImportApp
         {
             //Check which people are projector users
             PwsResourceElement[] pwsUsers = GetProjectorResource(people, sessionTicket);
-            //Map together relevant users
+            //Map together relevant users, could be improved
             Dictionary<Person, PwsResourceElement> userMap = new Dictionary<Person, PwsResourceElement>();
             foreach (Person person in people)
             {
--- TimeImportApp/Interfaces/IAPIHelper.cs
+++ TimeImportApp/Interfaces/IAPIHelper.cs
@@ -12,13 +12,6 @@ namespace TimeImportApp
     {
         string GetSessionTicket(string accountCode, string userName, string password);  
         bool AuthenticateUser(string accountCode, string userName, string password);
-        PwsSaveTimeCardsRs SaveTimeCard(string sessionKey, PwsTimecardDetail timeCard, PwsUserElement userDetails);
-        PwsTimecardDetail GenerateNewTimeCard();
-        int CalculateTimeOffInYearToDate(PwsGetTimeCardsRs timeCard);
-        PwsGetTimeCardsRs GetExistingTimeCard(string sessionKey, PwsUserElement userDetails);
-        string GenerateSessionKey(IPwsProjectorServices pwsProjectorServices, string username, string password, string accountCode);
-        PwsGetExpenseReportsRs GetExpenseReport(string sessionKey);
-        PwsUserElement GetUserDetails(string userName, string sessionKey);
         bool AddTimeCards(List<Person> people, string sessionTicket);
         List<Person> GetErrors();
     }   
